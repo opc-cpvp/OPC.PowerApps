499265d5908f9559bd1aa77cbf39fe0668ca6369
Joshua Campbell <joshua.campbell@priv.gc.ca>
2019-10-09 10:44:41 -0400

    Solution management.

---
+++ XrmDefinitelyTyped/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XrmDefinitelyTyped
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            // This method was intentionally left empty to allow for the application to compile.
+        }
+    }
+}
