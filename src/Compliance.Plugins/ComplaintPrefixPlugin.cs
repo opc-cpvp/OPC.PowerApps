eb5f000351ae339a01d4260a3434e88b6f77db3e
Joshua Campbell <joshua.campbell@priv.gc.ca>
2019-11-06 11:30:39 -0500

    - Added a utility script to generate earlybound entities - Added a plugin to prefix complaint numbers

---
+++ src/Compliance.Plugins/ComplaintPrefixPlugin.cs
@@ -0,0 +1,36 @@
+﻿using Compliance.Plugins.Entities;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Compliance.Plugins
+{
+    public partial class ComplaintPrefixPlugin : PluginBase
+    {
+        public ComplaintPrefixPlugin()
+            : base(typeof(ComplaintPrefixPlugin), runAsSystem: true)
+        {
+        }
+
+        protected override void ExecuteCrmPlugin(LocalPluginContext localContext)
+        {
+            if (localContext == null)
+                throw new InvalidPluginExecutionException("localContext", new ArgumentNullException(nameof(localContext)));
+
+            if (!(localContext.PluginExecutionContext.InputParameters["Target"] is opc_complaint complaint))
+                return;
+
+            try
+            {
+                var acroynym = complaint.opc_complaints_legislation.opc_acronym;
+                var number = complaint.opc_number;
+
+                complaint.opc_number = $"{acroynym}-{number}";
+            }
+            catch (Exception ex)
+            {
+                localContext.Trace($"Exception: {ex.Message} - Stack Trace: {ex.StackTrace}");
+                throw new InvalidPluginExecutionException($"An error occurred in the plug-in. ComplaintPrefix: {ex.Message}", ex);
+            }
+        }
+    }
+}
