2f2355f9b3f25779904c7cc2d5795b39de6a3230
Joshua Campbell <joshua.campbell@priv.gc.ca>
2020-01-08 10:11:32 -0500

    Added solution package script

---
+++ src/Compliance.Package/package_solution.ps1
@@ -0,0 +1,10 @@
+﻿$packageRoot = "..\packages"
+$solutionPackager = Get-ChildItem -Path $packageRoot -Filter "SolutionPackager.exe" -Recurse -ErrorAction SilentlyContinue -Force
+
+& $solutionPackager.FullName `
+	/action:Pack `
+	/zipfile:".\bin\Debug\PkgFolder\Compliance.zip" `
+	/folder:"..\Compliance.Solution\Solution" `
+	/map:"..\Compliance.Solution\map.xml" `
+	/packagetype:Both `
+	/nologo
