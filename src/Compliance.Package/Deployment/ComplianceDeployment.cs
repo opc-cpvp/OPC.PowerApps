e9f4ff3b4c5758d4bb780889b2fe91738550fcfc
Joshua Campbell <joshua.campbell@priv.gc.ca>
2020-01-14 16:52:28 -0500

    Remove and Sort usings

--- src/Compliance.Package/Deployment/ComplianceDeployment.cs
+++ src/Compliance.Package/Deployment/ComplianceDeployment.cs
@@ -7,8 +7,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
