634948c1a291779dffa220e79af9c1ca49182d63
Joshua Campbell <joshua.campbell@priv.gc.ca>
2020-02-12 16:48:05 -0500

    Refactored RiskAssessmentControl code.

--- src/Compliance.WebResources/src/controls/RiskAssessment/RiskAssessmentControl.ts
+++ src/Compliance.WebResources/src/controls/RiskAssessment/RiskAssessmentControl.ts
@@ -3,14 +3,188 @@ import "reflect-metadata";
 export namespace Controls {
 
     export class RiskAssessmentControl {
-        constructor() {
+        private readonly _placeholder: HTMLDivElement;
+        private readonly _riskAssessmentId: string;
+        private readonly _riskTable: HTMLTableElement;
+        private readonly _riskTableBody: HTMLTableSectionElement;
+
+        private _riskAppetites: (opc_RiskAppetite_Fixed & { opc_riskappetiteid: string; } & { opc_name: string; })[];
+        private _riskDefinitions: (
+            { opc_RiskAssessmentDefinitionTemplate: opc_RiskAssessmentDefinitionTemplate_Result; } &
+            { opc_RiskAssessmentFactorTemplate: opc_RiskAssessmentFactorTemplate_Result; } &
+            { opc_RiskAssessmentCategory: opc_RiskAssessmentCategory_Result; } &
+            opc_RiskAssessmentDefinition_Fixed &
+            { opc_riskassessmentdefinitionid: string } &
+            { opc_riskassessmentcategory_guid: string } &
+            { opc_riskassessmentfactortemplate_guid: string } &
+            { opc_riskassessmentdefinitiontemplate_guid: string } &
+            { opc_isselected: boolean }
+        )[];
+
+        constructor(placeholder: HTMLDivElement, riskAssessmentId: string) {
+            this._placeholder = placeholder;
+            this._riskAssessmentId = riskAssessmentId;
+            this._riskTable = document.createElement("table");
+            this._riskTableBody = this._riskTable.createTBody();
+
+            this._riskTable.classList.add("zui-table", "zui-table-highlight-all");
         }
 
-        /**
-         * Handle the form OnLoad event.
-         *
-         * @event OnLoad
-         */
+        public async initializeControl() {
+            const appetitePromise = this.loadRiskAppetites();
+            const definitionPromise = this.loadRiskDefinitions();
+
+            this._riskAppetites = await appetitePromise;
+            this._riskDefinitions = await definitionPromise;
+
+            this.render();
+        }
+
+        private async loadRiskAppetites() {
+            return XrmQuery.retrieveMultiple(x => x.opc_riskappetites)
+                .select(x => [x.opc_riskappetiteid, x.opc_name])
+                .filter(x => Filter.equals(x.statuscode, opc_riskappetite_statuscode.Active))
+                .orderDesc(x => x.opc_name)
+                .promise();
+        }
+
+        private async loadRiskDefinitions() {
+            return XrmQuery.retrieveRelatedMultiple(x => x.opc_riskassessments, this._riskAssessmentId, x => x.opc_riskassessment_riskassessmentdefinitions_riskassessment)
+                .select(x => [x.opc_riskassessmentdefinitionid, x.opc_riskassessmentcategory_guid, x.opc_riskassessmentfactortemplate_guid, x.opc_riskassessmentdefinitiontemplate_guid, x.opc_isselected])
+                .expand(x => x.opc_RiskAssessmentCategory, x => [x.opc_name, x.opc_sequence])
+                .expand(x => x.opc_RiskAssessmentFactorTemplate, x => [x.opc_name, x.opc_sequence])
+                .expand(x => x.opc_RiskAssessmentDefinitionTemplate, x => [x.opc_name, x.opc_riskappetite_guid])
+                .filter(x => Filter.equals(x.statuscode, opc_riskassessmentdefinition_statuscode.Active))
+                .promise();
+        }
+
+        private getCategories() {
+            return this._riskDefinitions.map(x => x.opc_RiskAssessmentCategory)
+                .filter((value, index, array) => {
+                    return array.map(x => x.opc_riskassessmentcategoryid).indexOf(value.opc_riskassessmentcategoryid) === index;
+                })
+                .sort((a, b) => {
+                    if (a.opc_sequence > b.opc_sequence) return 1;
+                    if (a.opc_sequence < b.opc_sequence) return -1;
+                });
+        }
+
+        private getCategoryFactors(category: opc_RiskAssessmentCategory_Result) {
+            return this._riskDefinitions.filter(x => x.opc_riskassessmentcategory_guid === category.opc_riskassessmentcategoryid)
+                .map(x => x.opc_RiskAssessmentFactorTemplate)
+                .filter((value, index, array) => {
+                    return array.map(x => x.opc_riskassessmentfactortemplateid).indexOf(value.opc_riskassessmentfactortemplateid) === index;
+                })
+                .sort((a, b) => {
+                    if (a.opc_sequence > b.opc_sequence) return 1;
+                    if (a.opc_sequence < b.opc_sequence) return -1;
+                });
+        }
+
+        private getCategoryFactorDefinitions(category: opc_RiskAssessmentCategory_Result, factor: opc_RiskAssessmentFactorTemplate_Result) {
+            return this._riskDefinitions.filter(x => x.opc_riskassessmentcategory_guid === category.opc_riskassessmentcategoryid && x.opc_riskassessmentfactortemplate_guid === factor.opc_riskassessmentfactortemplateid);
+        }
+
+        private render() {
+            this.renderHeader();
+            this.renderBody();
+
+            this._placeholder.appendChild(this._riskTable);
+        }
+
+        private renderHeader() {
+            const tableHead = this._riskTable.createTHead();
+            const tableRow = tableHead.insertRow();
+
+            const appetites = this._riskAppetites.map(x => x.opc_name);
+            const headers: string[] = ["Risk Measurement Categories", "Factors"].concat(appetites);
+
+            for (let header of headers) {
+                const tableHeader = document.createElement("th");
+                tableHeader.textContent = header
+                tableRow.appendChild(tableHeader);
+            }
+        }
+
+        private renderBody() {
+            let categories = this.getCategories();
+
+            for (let category of categories) {
+                let factors = this.getCategoryFactors(category);
+                this.renderCategory(category, factors);
+            }
+        }
+
+        private renderCategory(category: opc_RiskAssessmentCategory_Result, factors: opc_RiskAssessmentFactorTemplate_Result[]) {
+            for (let i = 0; i < factors.length; i++) {
+                let factor = factors[i];
+
+                let tableRow = this._riskTableBody.insertRow();
+
+                if (i == 0) {
+                    let categoryHeader = document.createElement("th");
+                    categoryHeader.textContent = category.opc_name;
+                    categoryHeader.rowSpan = factors.length;
+                    categoryHeader.scope = "row";
+                    tableRow.appendChild(categoryHeader);
+                }
+
+                let factorHeader = document.createElement("th");
+                factorHeader.textContent = factor.opc_name;
+                factorHeader.scope = "row";
+                tableRow.appendChild(factorHeader);
+
+                let definitions = this.getCategoryFactorDefinitions(category, factor);
+                for (let j = 0; j < this._riskAppetites.length; j++) {
+                    const appetite = this._riskAppetites[j];
+                    const definition = definitions.find(x => x.opc_RiskAssessmentDefinitionTemplate.opc_riskappetite_guid === appetite.opc_riskappetiteid);
+
+                    if (!definition) {
+                        tableRow.insertCell();
+                        continue;
+                    }
+
+                    const definitionCell = tableRow.insertCell();
+                    const matchingDefinitions = definitions.filter(x => x.opc_RiskAssessmentDefinitionTemplate.opc_name === definition.opc_RiskAssessmentDefinitionTemplate.opc_name).length;
+
+                    definitionCell.textContent = definition.opc_RiskAssessmentDefinitionTemplate.opc_name;
+                    definitionCell.colSpan = matchingDefinitions;
+
+                    if (definition.opc_isselected)
+                        definitionCell.className = "is-selected";
+
+                    definitionCell.setAttribute("data-guid", definition.opc_riskassessmentdefinitionid);
+                    definitionCell.onclick = this.onDefinitionClick;
+
+                    j += matchingDefinitions - 1;
+                }
+            }
+        }
+
+        private onDefinitionClick(this: GlobalEventHandlers, ev: MouseEvent) {
+            const cell = <HTMLTableCellElement>ev.target;
+
+            const isSelected = !cell.classList.contains("is-selected");
+            const definitionId = cell.getAttribute("data-guid");
+
+            const currentRow = cell.closest("tr");
+            const selectedCell = currentRow.querySelector("td[data-guid].is-selected");
+
+            if (selectedCell) {
+                const selectedDefinitionId = selectedCell.getAttribute("data-guid");
+                XrmQuery.update(x => x.opc_riskassessmentdefinitions, selectedDefinitionId, { opc_isselected: false }).execute(id => {
+                    selectedCell.classList.toggle("is-selected");
+                });
+
+                if (selectedDefinitionId === definitionId)
+                    return;
+            }
+
+            XrmQuery.update(x => x.opc_riskassessmentdefinitions, definitionId, { opc_isselected: isSelected }).execute(id => {
+                cell.classList.toggle("is-selected");
+            });
+        }
+        
         public static control_OnLoad(): void {
             let parameters = Xrm.Utility.getGlobalContext().getQueryStringParameters();
 
@@ -20,124 +194,11 @@ export namespace Controls {
                 .filter(x => Filter.equals(x.statuscode, opc_riskassessment_statuscode.Active))
                 .promise()
                 .then((riskAssessments) => {
-                    for(let riskAssessment of riskAssessments) {
-                        let riskAssessmentId = riskAssessment.opc_riskassessmentid;
+                    for (let riskAssessment of riskAssessments) {
+                        let placeholder = <HTMLDivElement>document.getElementById("risks");
 
-                        XrmQuery.retrieveMultiple(x => x.opc_riskappetites)
-                            .select(x => [x.opc_riskappetiteid, x.opc_name])
-                            .orderDesc(x => x.opc_name)
-                            .promise()
-                            .then((riskAppetites) => {
-                                let riskTable = <HTMLTableElement> document.getElementById("risks");
-
-                                XrmQuery.retrieveRelatedMultiple(x => x.opc_riskassessments, riskAssessmentId, x => x.opc_riskassessment_riskassessmentdefinitions_riskassessment)
-                                    .select(x => [x.opc_riskassessmentdefinitionid, x.opc_riskassessmentcategory_guid, x.opc_riskassessmentfactortemplate_guid, x.opc_riskassessmentdefinitiontemplate_guid, x.opc_isselected])
-                                    .expand(x => x.opc_RiskAssessmentCategory, x => [x.opc_name, x.opc_sequence])
-                                    .expand(x => x.opc_RiskAssessmentFactorTemplate, x => [x.opc_name, x.opc_sequence])
-                                    .expand(x => x.opc_RiskAssessmentDefinitionTemplate, x => [x.opc_name, x.opc_riskappetite_guid])
-                                    .filter(x => Filter.equals(x.statuscode, opc_riskassessmentdefinition_statuscode.Active))
-                                    .promise()
-                                    .then((riskDefinitions) => {
-                                        let currentCategory = "";
-                                        let categories = riskDefinitions.map(x => x.opc_RiskAssessmentCategory)
-                                            .filter((value, index, array) => {
-                                                return array.map(x => x.opc_riskassessmentcategoryid).indexOf(value.opc_riskassessmentcategoryid) === index;
-                                            })
-                                            .sort((a, b) => {
-                                                if (a.opc_sequence > b.opc_sequence) return 1;
-                                                if (a.opc_sequence < b.opc_sequence) return -1;
-                                            });
-
-                                        for (let category of categories) {                                            
-                                            let currentFactor = "";
-                                            let previousDefinition = "";
-                                            let factors = riskDefinitions.filter(x => x.opc_riskassessmentcategory_guid === category.opc_riskassessmentcategoryid)
-                                                .map(x => x.opc_RiskAssessmentFactorTemplate)
-                                                .filter((value, index, array) => {
-                                                    return array.map(x => x.opc_riskassessmentfactortemplateid).indexOf(value.opc_riskassessmentfactortemplateid) === index;
-                                                })
-                                                .sort((a, b) => {
-                                                    if (a.opc_sequence > b.opc_sequence) return 1;
-                                                    if (a.opc_sequence < b.opc_sequence) return -1;
-                                                });
-
-                                            for (let factor of factors) {
-                                                let definitions = riskDefinitions.filter(x => x.opc_riskassessmentcategory_guid === category.opc_riskassessmentcategoryid && x.opc_riskassessmentfactortemplate_guid === factor.opc_riskassessmentfactortemplateid);
-
-                                                let tableRow = riskTable.tBodies[0].insertRow();
-
-                                                if (currentCategory !== category.opc_name) {
-                                                    currentCategory = category.opc_name;
-
-                                                    let categoryHeader = document.createElement("th");
-                                                    categoryHeader.textContent = currentCategory;
-                                                    categoryHeader.rowSpan = factors.length;
-                                                    categoryHeader.scope = "row";
-                                                    tableRow.appendChild(categoryHeader);
-                                                }
-
-                                                if (currentFactor !== factor.opc_name) {
-                                                    currentFactor = factor.opc_name;
-
-                                                    let factorHeader = document.createElement("th");
-                                                    factorHeader.textContent = currentFactor;
-                                                    factorHeader.scope = "row";
-                                                    tableRow.appendChild(factorHeader);
-                                                }
-
-                                                for (let appetite of riskAppetites) {
-                                                    let riskDefinitions = definitions.filter(x => x.opc_RiskAssessmentDefinitionTemplate.opc_riskappetite_guid === appetite.opc_riskappetiteid);
-                                                    let riskDefinition = riskDefinitions[0];
-
-                                                    if (riskDefinitions.length < 1) {
-                                                        tableRow.insertCell();
-                                                        continue;
-                                                    }
-
-                                                    if (previousDefinition === riskDefinition.opc_RiskAssessmentDefinitionTemplate.opc_name)
-                                                        continue;
-
-                                                    let definitionCell = tableRow.insertCell();
-
-                                                    previousDefinition = riskDefinition.opc_RiskAssessmentDefinitionTemplate.opc_name;
-
-                                                    let matchingDefinitions = definitions.filter(x => x.opc_RiskAssessmentDefinitionTemplate.opc_name === riskDefinition.opc_RiskAssessmentDefinitionTemplate.opc_name).length;
-
-                                                    definitionCell.textContent = riskDefinition.opc_RiskAssessmentDefinitionTemplate.opc_name;
-                                                    definitionCell.colSpan = matchingDefinitions;
-
-                                                    if (riskDefinition.opc_isselected)
-                                                        definitionCell.className = "is-selected";
-
-                                                    definitionCell.setAttribute("data-guid", riskDefinition.opc_riskassessmentdefinitionid);
-                                                    definitionCell.onclick = function(this, ev) {
-                                                        let cell = <HTMLTableCellElement> ev.target;
-
-                                                        let isSelected = !cell.classList.contains("is-selected");
-                                                        let riskDefinitionGuid = cell.getAttribute("data-guid");
-
-                                                        let row = cell.closest("tr");
-                                                        let selectedCell = row.querySelector("td[data-guid].is-selected");
-
-                                                        if (selectedCell) {
-                                                            let selectedGuid = selectedCell.getAttribute("data-guid");
-                                                            XrmQuery.update(x => x.opc_riskassessmentdefinitions, selectedGuid, { opc_isselected: false }).execute(id => {
-                                                                selectedCell.classList.toggle("is-selected");
-                                                            });
-
-                                                            if (selectedGuid === riskDefinitionGuid)
-                                                                return;
-                                                        }
-
-                                                        XrmQuery.update(x => x.opc_riskassessmentdefinitions, riskDefinitionGuid, { opc_isselected: isSelected }).execute(id => {
-                                                            cell.classList.toggle("is-selected");
-                                                        });
-                                                    };
-                                                }
-                                            }
-                                        }
-                                    });
-                            });
+                        let control = new RiskAssessmentControl(placeholder, riskAssessment.opc_riskassessmentid)
+                        control.initializeControl();
                     }
                 });
         }
