af1a1a412a88f3e22fb21c18c0ac6de5a6ade5c1
pierluc-ssoucy <pierluc.ssoucy@gmail.com>
2020-03-10 11:14:19 -0400

    Parameterized for pipeline execution

--- src/CrmSvcUtil.Filter/generate_optionset_enums.ps1
+++ src/CrmSvcUtil.Filter/generate_optionset_enums.ps1
@@ -1,10 +1,24 @@
-﻿$projectOutput = "bin\Debug"
-$crmSvcUtil = Get-ChildItem -Path $projectOutput -Filter "CrmSvcUtil.exe" -Recurse -ErrorAction SilentlyContinue -Force
+Param([switch]$NonInteractive = $false, [string]$UserName, [string]$Password, [string]$Url)
 
-& $crmSvcUtil.FullName `
-	/interactivelogin `
-	/out:"../Compliance.Entities/OptionSets.cs" `
-	/namespace:"Compliance.Entities" `
-	/codewriterfilter:"CrmSvcUtil.Filter.GenerateOptionSetEnums.FilteringService, CrmSvcUtil.Filter" `
-	/codecustomization:"CrmSvcUtil.Filter.GenerateOptionSetEnums.CodeCustomizationService, CrmSvcUtil.Filter" `
-	/namingservice:"CrmSvcUtil.Filter.GenerateOptionSetEnums.NamingService, CrmSvcUtil.Filter"
+$projectOutput = "bin\Debug"
+$crmSvcUtil = Get-ChildItem -Path $projectOutput -Filter "CrmSvcUtil.exe" -Recurse -ErrorAction SilentlyContinue -Force | select -first 1 -ExpandProperty FullName
+
+if($NonInteractive){
+	& $crmSvcUtil `
+		/username:"$username" `
+		/password:"$password" `
+		/url:"$url/XRMServices/2011/Organization.svc" `
+		/out:"../Compliance.Entities/Entities.cs" `
+		/namespace:"Compliance.Entities" `
+		/codewriterfilter:"CrmSvcUtil.Filter.GenerateOptionSetEnums.FilteringService, CrmSvcUtil.Filter" `
+		/codecustomization:"CrmSvcUtil.Filter.GenerateOptionSetEnums.CodeCustomizationService, CrmSvcUtil.Filter" `
+		/namingservice:"CrmSvcUtil.Filter.GenerateOptionSetEnums.NamingService, CrmSvcUtil.Filter"
+} else {
+	& $crmSvcUtil `
+		/interactivelogin `
+		/out:"../Compliance.Entities/Entities.cs" `
+		/namespace:"Compliance.Entities" `
+		/codewriterfilter:"CrmSvcUtil.Filter.GenerateOptionSetEnums.FilteringService, CrmSvcUtil.Filter" `
+		/codecustomization:"CrmSvcUtil.Filter.GenerateOptionSetEnums.CodeCustomizationService, CrmSvcUtil.Filter" `
+		/namingservice:"CrmSvcUtil.Filter.GenerateOptionSetEnums.NamingService, CrmSvcUtil.Filter"
+}
