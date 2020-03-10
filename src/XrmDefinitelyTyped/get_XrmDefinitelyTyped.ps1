f32ce83297db93f100506c2ba8eeab4da1a939db
pierluc-ssoucy <pierluc.ssoucy@gmail.com>
2020-03-10 11:12:18 -0400

    Parameterized xrmdefinitelytyped for pipeline execution

--- src/XrmDefinitelyTyped/get_XrmDefinitelyTyped.ps1
+++ src/XrmDefinitelyTyped/get_XrmDefinitelyTyped.ps1
@@ -1,35 +1,53 @@
-﻿# Prompt for credentials
-$cred = Get-Credential
+Param([switch]$NonInteractive = $false, [string]$UserName, [string]$Password, [string]$Solution,[string]$Url)
 
-# Prompt for organization
-if(!(Get-Module -Name Microsoft.Xrm.Data.PowerShell)){
-	Install-Module -Name Microsoft.Xrm.Data.PowerShell -Force -AllowClobber
+$xdtexe = Get-ChildItem "../packages" -Filter XrmDefinitelyTyped.exe -recurse | select -first 1 -ExpandProperty FullName
+
+if($NonInteractive){
+
+	# Export XRM TypeScript definitions
+    & $xdtexe /url:$Url"/XRMServices/2011/Organization.svc" `
+      /out:"../$Solution.WebResources/@types/xrmdefinitelytyped" `
+      /jsLib:"../$Solution.WebResources/js/lib/xrmquery" `
+      /web:"" `
+      /solutions:"$Solution" `
+      /username:"$UserName" `
+      /password:"$Password" `
+      /oneFile:"true"
+
+} else {
+	# Prompt for credentials
+	$cred = Get-Credential
+
+	# Prompt for organization
+	if(!(Get-Module -Name Microsoft.Xrm.Data.PowerShell)){
+		Install-Module -Name Microsoft.Xrm.Data.PowerShell -Force -AllowClobber
+	}
+	Connect-CrmOnlineDiscovery $cred
+
+	# Set the connection timeout to 5 minutes
+	Set-CrmConnectionTimeout -conn $conn -TimeoutInSeconds 300
+
+	# Retrieve list of solutions
+	$solutions = (Get-CrmRecords -conn $conn -EntityLogicalName solution -FilterAttribute isvisible -FilterOperator eq -FilterValue 1 -Fields *).CrmRecords | Select-Object -Property friendlyname, uniquename
+
+	# Display list of available solutions
+	for ($i = 0; $i -lt $solutions.Length; $i++) {
+		$solution = $solutions[$i]
+		Write-Host("[{0}] {1}" -f $i, $solution.friendlyname)
+	}
+	Write-Host("")
+
+	# Prompt for solution
+	$i = Read-Host -Prompt "Select CRM Solution by index number"
+	$solution = $solutions[$i].uniquename
+
+	# Export XRM TypeScript definitions
+	& $xdtexe /url:$($conn.ConnectedOrgPublishedEndpoints["OrganizationService"]) `
+		/out:$('"../{0}.WebResources/@types/xrmdefinitelytyped"' -f $solution) `
+		/jsLib:$('"../{0}.WebResources/js/lib/xrmquery"' -f $solution) `
+		/web:"" `
+		/solutions:$($solution) `
+		/username:$($cred.Username) `
+		/password:$($cred.GetNetworkCredential().Password) `
+		/oneFile:"true"
 }
-Connect-CrmOnlineDiscovery $cred
-
-# Set the connection timeout to 5 minutes
-Set-CrmConnectionTimeout -conn $conn -TimeoutInSeconds 300
-
-# Retrieve list of solutions
-$solutions = (Get-CrmRecords -conn $conn -EntityLogicalName solution -FilterAttribute isvisible -FilterOperator eq -FilterValue 1 -Fields *).CrmRecords | Select-Object -Property friendlyname, uniquename
-
-# Display list of available solutions
-for ($i = 0; $i -lt $solutions.Length; $i++) {
-	$solution = $solutions[$i]
-	Write-Host("[{0}] {1}" -f $i, $solution.friendlyname)
-}
-Write-Host("")
-
-# Prompt for solution
-$i = Read-Host -Prompt "Select CRM Solution by index number"
-$solution = $solutions[$i].uniquename
-
-# Export XRM TypeScript definitions
-XrmDefinitelyTyped\XrmDefinitelyTyped.exe /url:$($conn.ConnectedOrgPublishedEndpoints["OrganizationService"]) `
-    /out:$('"../{0}.WebResources/@types/xrmdefinitelytyped"' -f $solution) `
-	/jsLib:$('"../{0}.WebResources/js/lib/xrmquery"' -f $solution) `
-	/web:"" `
-	/solutions:$($solution) `
-	/username:$($cred.Username) `
-	/password:$($cred.GetNetworkCredential().Password) `
-	/oneFile:"true"
