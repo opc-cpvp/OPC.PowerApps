9a72312b33f483a3568eec3db44f3901369fd984
pierluc-ssoucy <pierluc.ssoucy@gmail.com>
2020-03-10 11:13:37 -0400

    Parameterized for pipeline execution

--- src/CrmSvcUtil.Filter/generate_earlybound_entities.ps1
+++ src/CrmSvcUtil.Filter/generate_earlybound_entities.ps1
@@ -1,7 +1,18 @@
-﻿$projectOutput = "bin\Debug"
-$crmSvcUtil = Get-ChildItem -Path $projectOutput -Filter "CrmSvcUtil.exe" -Recurse -ErrorAction SilentlyContinue -Force
+Param([switch]$NonInteractive = $false, [string]$UserName, [string]$Password, [string]$Url)
 
-& $crmSvcUtil.FullName `
-	/interactivelogin `
-	/out:"../Compliance.Entities/Entities.cs" `
-	/namespace:"Compliance.Entities"
+$projectOutput = "bin\Debug"
+$crmSvcUtil = Get-ChildItem -Path $projectOutput -Filter "CrmSvcUtil.exe" -Recurse -ErrorAction SilentlyContinue -Force | select -first 1 -ExpandProperty FullName
+
+if($NonInteractive){
+	& $crmSvcUtil `
+		/url:"$url/XRMServices/2011/Organization.svc" `
+		/out:"../Compliance.Entities/Entities.cs" `
+		/namespace:"Compliance.Entities" `
+		/username:"$username" `
+		/password:"$password"
+} else {
+	& $crmSvcUtil `
+		/interactivelogin `
+		/out:"../Compliance.Entities/Entities.cs" `
+		/namespace:"Compliance.Entities"
+}
