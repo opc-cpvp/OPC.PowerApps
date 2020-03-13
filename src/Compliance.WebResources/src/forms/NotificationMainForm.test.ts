5127a9d7bc7f0533e35c4488fd63152ba5da0581
Jonathan Thomassin <jonathan.thomassin@priv.gc.ca>
2020-03-13 10:29:23 -0400

    Added more description to the tests.

--- src/Compliance.WebResources/src/forms/NotificationMainForm.test.ts
+++ src/Compliance.WebResources/src/forms/NotificationMainForm.test.ts
@@ -20,103 +20,104 @@ function ReplaceFunctions(service: NotificationService, mockContext: XrmExecutio
 
     sandbox.replace(service, "markAsRead", markAsRead);
 }
+describe("Notification", () => {
+    describe("when form is loading", () => {
+        let service: NotificationService;
+        let form: Notification.Forms.MainForm;
+        let mockContext: XrmExecutionContextMock<Form.opc_notification.Main.Information, any>;
+        let mockNavigation: XrmNavigationMock;
+        let contextSpy: any;
+        let serviceSpy: any;
+        let navigationSpy: any;
 
-describe("when form is loading", () => {
-    let service: NotificationService;
-    let form: Notification.Forms.MainForm;
-    let mockContext: XrmExecutionContextMock<Form.opc_notification.Main.Information, any>;
-    let mockNavigation: XrmNavigationMock;
-    let contextSpy: any;
-    let serviceSpy: any;
-    let navigationSpy: any;
+        beforeEach(function () {
+            service = new NotificationService();
+            mockContext = new XrmSaveEventContextMock<Form.opc_notification.Main.Information>();
+            mockNavigation = new XrmNavigationMock();
+            form = new Notification.Forms.MainForm(service, mockNavigation);
+            contextSpy = sandbox.spy(mockContext);
+            serviceSpy = sandbox.spy(service);
+            navigationSpy = sandbox.spy(mockNavigation);
+        });
 
-    beforeEach(function () {
-        service = new NotificationService();
-        mockContext = new XrmSaveEventContextMock<Form.opc_notification.Main.Information>();
-        mockNavigation = new XrmNavigationMock();
-        form = new Notification.Forms.MainForm(service, mockNavigation);
-        contextSpy = sandbox.spy(mockContext);
-        serviceSpy = sandbox.spy(service);
-        navigationSpy = sandbox.spy(mockNavigation);
-    });
+        afterEach(function () {
+            sandbox.restore();
+        });
 
-    afterEach(function () {
-        sandbox.restore();
-    });
+        it("it should display Complaint field if it is related to a Complaint", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_complaintid").setValue("PA-000000");
+            mockContext.getFormContext().getControl("opc_complaintid").setVisible(false);
+            ReplaceFunctions(service, mockContext);
 
-    it("it should display Complaint field if it is related to a Complaint", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_complaintid").setValue("PA-000000");
-        mockContext.getFormContext().getControl("opc_complaintid").setVisible(false);
-        ReplaceFunctions(service, mockContext);
+            // Act
+            form.initializeComponents(mockContext);
 
-        // Act
-        form.initializeComponents(mockContext);
+            // Assert
+            contextSpy.getFormContext().getControl("opc_complaintid").getVisible().should.equal(true);
+        });
 
-        // Assert
-        contextSpy.getFormContext().getControl("opc_complaintid").getVisible().should.equal(true);
-    });
+        it("it should not display Complaint field if it is not related to a Complaint", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_complaintid").setValue(null);
+            mockContext.getFormContext().getControl("opc_complaintid").setVisible(false);
+            ReplaceFunctions(service, mockContext);
 
-    it("it should not display Complaint field if it is not related to a Complaint", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_complaintid").setValue(null);
-        mockContext.getFormContext().getControl("opc_complaintid").setVisible(false);
-        ReplaceFunctions(service, mockContext);
+            // Act
+            form.initializeComponents(mockContext);
 
-        // Act
-        form.initializeComponents(mockContext);
+            // Assert
+            contextSpy.getFormContext().getControl("opc_complaintid").getVisible().should.equal(false);
+        });
 
-        // Assert
-        contextSpy.getFormContext().getControl("opc_complaintid").getVisible().should.equal(false);
-    });
+        it("it should call markAsRead", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("statecode").setValue(opc_notification_statecode.Active);
+            mockContext.getFormContext().getAttribute("statuscode").setValue(opc_notification_statuscode.Unread);
+            ReplaceFunctions(service, mockContext);
 
-    it("it should call markAsRead", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("statecode").setValue(opc_notification_statecode.Active);
-        mockContext.getFormContext().getAttribute("statuscode").setValue(opc_notification_statuscode.Unread);
-        ReplaceFunctions(service, mockContext);
+            // Act
+            form.initializeComponents(mockContext);
 
-        // Act
-        form.initializeComponents(mockContext);
+            // Assert
+            serviceSpy.markAsRead.should.have.been.called;
+        });
 
-        // Assert
-        serviceSpy.markAsRead.should.have.been.called;
-    });
+        it("it should change the status reason from unread to read", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("statecode").setValue(opc_notification_statecode.Active);
+            mockContext.getFormContext().getAttribute("statuscode").setValue(opc_notification_statuscode.Unread);
+            ReplaceFunctions(service, mockContext);
 
-    it("it should change the status reason from unread to read", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("statecode").setValue(opc_notification_statecode.Active);
-        mockContext.getFormContext().getAttribute("statuscode").setValue(opc_notification_statuscode.Unread);
-        ReplaceFunctions(service, mockContext);
+            // Act
+            form.initializeComponents(mockContext);
 
-        // Act
-        form.initializeComponents(mockContext);
+            // Assert
+            contextSpy.getFormContext().getAttribute("statuscode").getValue().should.equal(opc_notification_statuscode.Read);
+        });
 
-        // Assert
-        contextSpy.getFormContext().getAttribute("statuscode").getValue().should.equal(opc_notification_statuscode.Read);
-    });
+        it("it should redirect user to Complaint page if it is related to a Complaint", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_complaintid").setValue("PA-000000");
+            ReplaceFunctions(service, mockContext);
 
-    it("it should redirect user to Complaint page if it is related to a Complaint", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_complaintid").setValue("PA-000000");
-        ReplaceFunctions(service, mockContext);
+            // Act
+            form.initializeComponents(mockContext);
 
-        // Act
-        form.initializeComponents(mockContext);
+            // Assert
+            navigationSpy.openForm.should.have.been.called;
+        });
 
-        // Assert
-        navigationSpy.openForm.should.have.been.called;
-    });
+        it("it should not redirect user to Complaint page if it is not related to a Complaint", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_complaintid").setValue(null);
+            ReplaceFunctions(service, mockContext);
 
-    it("it should not redirect user to Complaint page if it is not related to a Complaint", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_complaintid").setValue(null);
-        ReplaceFunctions(service, mockContext);
+            // Act
+            form.initializeComponents(mockContext);
 
-        // Act
-        form.initializeComponents(mockContext);
-
-        // Assert
-        navigationSpy.openForm.should.not.have.been.called;
+            // Assert
+            navigationSpy.openForm.should.not.have.been.called;
+        });
     });
 });
--- src/Compliance.WebResources/src/forms/ReminderMainForm.test.ts
+++ src/Compliance.WebResources/src/forms/ReminderMainForm.test.ts
@@ -49,161 +49,162 @@ describe("when form is loading", () => {
         contextSpy.getFormContext().ui.tabs.get("tab_general").sections.get("section_additionalusers").getVisible().should.equal(false);
     });
 });
+describe("Reminder", () => {
+    describe("when form is saving", () => {
+        let service: ReminderService;
+        let form: Reminder.Forms.MainForm;
+        let mockContext: XrmExecutionContextMock<Form.opc_reminder.Main.Information, any>;
+        let contextSpy: any;
+        let eventArgsSpy: any;
 
-describe("when form is saving", () => {
-    let service: ReminderService;
-    let form: Reminder.Forms.MainForm;
-    let mockContext: XrmExecutionContextMock<Form.opc_reminder.Main.Information, any>;
-    let contextSpy: any;
-    let eventArgsSpy: any;
+        beforeEach(function () {
+            service = new ReminderService();
+            form = new Reminder.Forms.MainForm(service);
+            mockContext = new XrmSaveEventContextMock<Form.opc_reminder.Main.Information>();
+            contextSpy = sandbox.spy(mockContext);
+            eventArgsSpy = sandbox.spy(mockContext.getEventArgs());
+        });
 
-    beforeEach(function () {
-        service = new ReminderService();
-        form = new Reminder.Forms.MainForm(service);
-        mockContext = new XrmSaveEventContextMock<Form.opc_reminder.Main.Information>();
-        contextSpy = sandbox.spy(mockContext);
-        eventArgsSpy = sandbox.spy(mockContext.getEventArgs());
+        afterEach(function () {
+            sandbox.restore();
+        });
+
+        it("it should display notifications if the user chooses to notify the case owner but no case is related to the reminder", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(true);
+            mockContext.getFormContext().getAttribute("opc_complaintid").setValue(null);
+
+            //We are calling initializeComponents to register the events and to be able to call save() on the entity, which will trigger the onsave event. Onsave is a private method.
+            form.initializeComponents(mockContext);
+
+            // Act
+            mockContext.getFormContext().data.entity.save();
+
+            // Assert
+            contextSpy.getFormContext().ui.getFormNotificationsLength().should.equal(1);
+            contextSpy.getFormContext().getControl("opc_notifycaseowner").getNotificationsLength().should.equal(1);
+        });
+
+        it("it should not display notifications if the user chooses to notify the case owner and has a case is related to the reminder", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(true);
+            mockContext.getFormContext().getAttribute("opc_complaintid").setValue("PA-000000");
+
+            //We are calling initializeComponents to register the events and to be able to call save() on the entity, which will trigger the onsave event. Onsave is a private method.
+            form.initializeComponents(mockContext);
+
+            // Act
+            mockContext.getFormContext().data.entity.save();
+
+            // Assert
+            contextSpy.getFormContext().ui.getFormNotificationsLength().should.equal(0);
+            contextSpy.getFormContext().getControl("opc_notifycaseowner").getNotificationsLength().should.equal(0);
+        });
+
+        it("it should display notifications if the user have not selected someone to be notified", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(false);
+            mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(false);
+            mockContext.getFormContext().getAttribute("opc_notifyme").setValue(false);
+            form.initializeComponents(mockContext);
+
+            // Act
+            mockContext.getFormContext().data.entity.save();
+
+            // Assert
+            contextSpy.getFormContext().ui.getFormNotificationsLength().should.equal(1);
+            contextSpy.getFormContext().getControl("opc_notifycaseowner").getNotificationsLength().should.equal(1);
+            contextSpy.getFormContext().getControl("opc_notifyme").getNotificationsLength().should.equal(1);
+            contextSpy.getFormContext().getControl("opc_notifyadditionalusers").getNotificationsLength().should.equal(1);
+        });
+
+        it("it should not display notifications if the user have selected someone to be notified", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(true);
+            mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(true);
+            mockContext.getFormContext().getAttribute("opc_notifyme").setValue(true);
+            mockContext.getFormContext().getAttribute("opc_complaintid").setValue("PA-000000");
+            form.initializeComponents(mockContext);
+
+            // Act
+            mockContext.getFormContext().data.entity.save();
+
+            // Assert
+            contextSpy.getFormContext().ui.getFormNotificationsLength().should.equal(0);
+            contextSpy.getFormContext().getControl("opc_notifycaseowner").getNotificationsLength().should.equal(0);
+            contextSpy.getFormContext().getControl("opc_notifyme").getNotificationsLength().should.equal(0);
+            contextSpy.getFormContext().getControl("opc_notifyadditionalusers").getNotificationsLength().should.equal(0);
+        });
+
+        it("it should cancel the save operation if the user have not selected someone to be notified", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(false);
+            mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(false);
+            mockContext.getFormContext().getAttribute("opc_notifyme").setValue(false);
+            form.initializeComponents(mockContext);
+
+            // Act
+            mockContext.getFormContext().data.entity.save();
+
+            // Assert
+            eventArgsSpy.preventDefault.should.have.been.called;
+        });
+
+        it("it should cancel the save operation if the user chooses to notify the case owner but no case is related to the reminder", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_complaintid").setValue(null);
+            form.initializeComponents(mockContext);
+
+            // Act
+            mockContext.getFormContext().data.entity.save();
+
+            // Assert
+            eventArgsSpy.preventDefault.should.have.been.called;
+        });
     });
 
-    afterEach(function () {
-        sandbox.restore();
-    });
+    describe("when fields value changes", () => {
+        let service: ReminderService;
+        let form: Reminder.Forms.MainForm;
+        let mockContext: XrmExecutionContextMock<Form.opc_reminder.Main.Information, any>;
+        let contextSpy: any;
 
-    it("it should display notifications if the user chooses to notify the case owner but no case is related to the reminder", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(true);
-        mockContext.getFormContext().getAttribute("opc_complaintid").setValue(null);
+        beforeEach(function () {
+            service = new ReminderService();
+            form = new Reminder.Forms.MainForm(service);
+            mockContext = new XrmSaveEventContextMock<Form.opc_reminder.Main.Information>();
+            contextSpy = sandbox.spy(mockContext);
+        });
 
-        //We are calling initializeComponents to register the events and to be able to call save() on the entity, which will trigger the onsave event. Onsave is a private method.
-        form.initializeComponents(mockContext);
+        afterEach(function () {
+            sandbox.restore();
+        });
 
-        // Act
-        mockContext.getFormContext().data.entity.save();
+        it("it should display the section to add additional users if the user chooses to notify additional users", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(true);
 
-        // Assert
-        contextSpy.getFormContext().ui.getFormNotificationsLength().should.equal(1);
-        contextSpy.getFormContext().getControl("opc_notifycaseowner").getNotificationsLength().should.equal(1);
-    });
+            //We are calling initializeComponents to register the events and to be able to call fireOnChange() on the attribute, which will trigger the onchange event. Onchange is a private method.
+            form.initializeComponents(mockContext);
 
-    it("it should not display notifications if the user chooses to notify the case owner and has a case is related to the reminder", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(true);
-        mockContext.getFormContext().getAttribute("opc_complaintid").setValue("PA-000000");
+            // Act
+            mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").fireOnChange();
 
-        //We are calling initializeComponents to register the events and to be able to call save() on the entity, which will trigger the onsave event. Onsave is a private method.
-        form.initializeComponents(mockContext);
+            // Assert
+            contextSpy.getFormContext().ui.tabs.get("tab_general").sections.get("section_additionalusers").getVisible().should.equal(true);
+        });
 
-        // Act
-        mockContext.getFormContext().data.entity.save();
+        it("it should hide the section to add additional users if the user chooses not to notify additional users", () => {
+            // Arrange
+            mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(false);
 
-        // Assert
-        contextSpy.getFormContext().ui.getFormNotificationsLength().should.equal(0);
-        contextSpy.getFormContext().getControl("opc_notifycaseowner").getNotificationsLength().should.equal(0);
-    });
+            form.initializeComponents(mockContext);
 
-    it("it should display notifications if the user have not selected someone to be notified", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(false);
-        mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(false);
-        mockContext.getFormContext().getAttribute("opc_notifyme").setValue(false);
-        form.initializeComponents(mockContext);
+            // Act
+            mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").fireOnChange();
 
-        // Act
-        mockContext.getFormContext().data.entity.save();
-
-        // Assert
-        contextSpy.getFormContext().ui.getFormNotificationsLength().should.equal(1);
-        contextSpy.getFormContext().getControl("opc_notifycaseowner").getNotificationsLength().should.equal(1);
-        contextSpy.getFormContext().getControl("opc_notifyme").getNotificationsLength().should.equal(1);
-        contextSpy.getFormContext().getControl("opc_notifyadditionalusers").getNotificationsLength().should.equal(1);
-    });
-
-    it("it should not display notifications if the user have selected someone to be notified", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(true);
-        mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(true);
-        mockContext.getFormContext().getAttribute("opc_notifyme").setValue(true);
-        mockContext.getFormContext().getAttribute("opc_complaintid").setValue("PA-000000");
-        form.initializeComponents(mockContext);
-
-        // Act
-        mockContext.getFormContext().data.entity.save();
-
-        // Assert
-        contextSpy.getFormContext().ui.getFormNotificationsLength().should.equal(0);
-        contextSpy.getFormContext().getControl("opc_notifycaseowner").getNotificationsLength().should.equal(0);
-        contextSpy.getFormContext().getControl("opc_notifyme").getNotificationsLength().should.equal(0);
-        contextSpy.getFormContext().getControl("opc_notifyadditionalusers").getNotificationsLength().should.equal(0);
-    });
-
-    it("it should cancel the save operation if the user have not selected someone to be notified", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(false);
-        mockContext.getFormContext().getAttribute("opc_notifycaseowner").setValue(false);
-        mockContext.getFormContext().getAttribute("opc_notifyme").setValue(false);
-        form.initializeComponents(mockContext);
-
-        // Act
-        mockContext.getFormContext().data.entity.save();
-
-        // Assert
-        eventArgsSpy.preventDefault.should.have.been.called;
-    });
-
-    it("it should cancel the save operation if the user chooses to notify the case owner but no case is related to the reminder", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_complaintid").setValue(null);
-        form.initializeComponents(mockContext);
-
-        // Act
-        mockContext.getFormContext().data.entity.save();
-
-        // Assert
-        eventArgsSpy.preventDefault.should.have.been.called;
-    });
-});
-
-describe("when fields value changes", () => {
-    let service: ReminderService;
-    let form: Reminder.Forms.MainForm;
-    let mockContext: XrmExecutionContextMock<Form.opc_reminder.Main.Information, any>;
-    let contextSpy: any;
-
-    beforeEach(function () {
-        service = new ReminderService();
-        form = new Reminder.Forms.MainForm(service);
-        mockContext = new XrmSaveEventContextMock<Form.opc_reminder.Main.Information>();
-        contextSpy = sandbox.spy(mockContext);
-    });
-
-    afterEach(function () {
-        sandbox.restore();
-    });
-
-    it("it should display the section to add additional users if the user chooses to notify additional users", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(true);
-
-        //We are calling initializeComponents to register the events and to be able to call fireOnChange() on the attribute, which will trigger the onchange event. Onchange is a private method.
-        form.initializeComponents(mockContext);
-
-        // Act
-        mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").fireOnChange();
-
-        // Assert
-        contextSpy.getFormContext().ui.tabs.get("tab_general").sections.get("section_additionalusers").getVisible().should.equal(true);
-    });
-
-    it("it should hide the section to add additional users if the user chooses not to notify additional users", () => {
-        // Arrange
-        mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").setValue(false);
-
-        form.initializeComponents(mockContext);
-
-        // Act
-        mockContext.getFormContext().getAttribute("opc_notifyadditionalusers").fireOnChange();
-
-        // Assert
-        contextSpy.getFormContext().ui.tabs.get("tab_general").sections.get("section_additionalusers").getVisible().should.equal(false);
+            // Assert
+            contextSpy.getFormContext().ui.tabs.get("tab_general").sections.get("section_additionalusers").getVisible().should.equal(false);
+        });
     });
 });
