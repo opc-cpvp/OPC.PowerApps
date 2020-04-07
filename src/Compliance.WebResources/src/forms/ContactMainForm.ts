12e19e827b6e55cfd41b4b49050d0b936b788d17
Jonathan Thomassin <jonathan.thomassin@priv.gc.ca>
2020-04-07 09:47:17 -0400

    Fixed not working JSON.parse in ContactMainForm.

--- src/Compliance.WebResources/src/forms/ContactMainForm.ts
+++ src/Compliance.WebResources/src/forms/ContactMainForm.ts
@@ -37,7 +37,10 @@ export namespace Contact.Forms {
             let userSecurityRoles = this._xrmUtility.getGlobalContext().userSettings.roles;
             let multipleComplaintStrategyValue = formContext.getAttribute("opc_multiplecomplaintstrategy").getValue();
 
-            userSecurityRoles.forEach(x => { if (JSON.parse(x).name === intakeManagerRoleName) isIntakeManager = true });
+            // TODO: I need to find a proper way to do this check instead of JSON.parse(JSON.stringify(x))
+            userSecurityRoles.forEach(x => {
+                if (JSON.parse(JSON.stringify(x)).name === intakeManagerRoleName) isIntakeManager = true
+            });
 
             if (!isIntakeManager && multipleComplaintStrategyValue !== opc_multiplecomplaintstrategy.Applied) {
                 formContext.getControl("opc_multiplecomplaintstrategy").removeOption(opc_multiplecomplaintstrategy.Applied);
