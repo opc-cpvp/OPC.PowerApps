5151044aae496cdd441a69f5e6b6f9241ff329a9
Pierluc Simplicio-Soucy <Pierluc.Simplicio-Soucy@priv.gc.ca>
2020-04-16 11:38:36 -0400

    adding in mock for usersettings language

--- src/Compliance.WebResources/test/XrmContextMock.ts
+++ src/Compliance.WebResources/test/XrmContextMock.ts
@@ -1,9 +1,14 @@
 /// <reference path="../@types/xrmdefinitelytyped/xrm.d.ts" />
 /// <reference path="../@types/xrmextensions/xrmextensions.d.ts" />
+import { XrmUserSettingsMock } from "./XrmUserSettingsMock";
 
 export class XrmContextMock implements Xrm.context {
 
     private _kvps: any;
+
+    constructor() {
+        this.userSettings = new XrmUserSettingsMock();
+    }
 
     /* NEW MEMBERS TO HELP MOCKING */
     setQueryStringParameters(keyvalues: any) {
---
+++ src/Compliance.WebResources/test/XrmUserSettingsMock.ts
@@ -0,0 +1,26 @@
+﻿import { XrmBaseControlMock } from "./XrmBaseControlMock";
+import { XrmAttributeMock } from "./XrmAttributeMock";
+
+export class XrmUserSettingsMock implements Xrm.userSettings {
+
+    constructor() {
+        // Setting default
+        this.languageId = 1033; // English
+    }
+
+    dateFormattingInfo: Xrm.dateFormattingInfo;
+    defaultDashboardId: string;
+    isGuidedHelpEnabled: boolean;
+    isHighContrastEnabled: boolean;
+    isRTL: boolean;
+    languageId: number;
+    securityRolePrivileges: string[];
+    securityRoles: string[];
+    userName: string;
+    getTimeZoneOffsetMinutes(): number {
+        throw new Error("Method not implemented.");
+    }
+    userId: string;
+
+
+}
