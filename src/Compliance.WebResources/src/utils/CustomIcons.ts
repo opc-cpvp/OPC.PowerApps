1814ecbda58966d9b03ce08d1581682f6570c9ca
Jonathan Thomassin <jonathan.thomassin@priv.gc.ca>
2020-04-17 14:28:53 -0400

    Changes to CustomIcons images name.

--- src/Compliance.WebResources/src/utils/CustomIcons.ts
+++ src/Compliance.WebResources/src/utils/CustomIcons.ts
@@ -8,7 +8,7 @@ export namespace Utilities {
 
             switch (parseInt(coldata, 10)) {
                 case 1:  //Unread = 1
-                    imgName = "opc_icon64x64_unread_notification.png";
+                    imgName = "opc_compliance/imgs/icon64x64_unread_notification.png";
                     switch (userLCID) {
                         case 1036:
                             tooltip = "Notification non lue";
@@ -19,7 +19,7 @@ export namespace Utilities {
                     }
                     break;
                 case 924340000:  //Read = 924,340,000
-                    imgName = "opc_icon64x64_read_notification.png";
+                    imgName = "opc_compliance/imgs/icon64x64_read_notification.png";
                     switch (userLCID) {
                         case 1036:
                             tooltip = "Notification lue";
@@ -35,39 +35,6 @@ export namespace Utilities {
 
             let resultarray = [imgName, tooltip];
             return resultarray;
-            //try {
-            //    switch (parseInt(coldata, 10)) {
-            //        case 1:  //Unread = 1
-            //            imgName = "opc_icon64x64_unread_notification.png";
-            //            switch (userLCID) {
-            //                case 1036:
-            //                    tooltip = "Notification non lue";
-            //                    break;
-            //                default:
-            //                    tooltip = "Unread notification";
-            //                    break;
-            //            }
-            //            break;
-            //        case 924340000:  //Read = 924,340,000
-            //            imgName = "opc_icon64x64_read_notification.png";
-            //            switch (userLCID) {
-            //                case 1036:
-            //                    tooltip = "Notification lue";
-            //                    break;
-            //                default:
-            //                    tooltip = "Read notification";
-            //                    break;
-            //            }
-            //            break;
-            //        default:
-            //            break;
-            //    }
-            //} catch (error) {
-
-            //} finally {
-            //    var resultarray = [imgName, tooltip];
-            //    return resultarray;
-            //}
         }
     }
 }
