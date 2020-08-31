0cba487ce112e0cb961c724c8ed883390e04360e
Gabriel Fournier Grenier <Gabriel.FournierGrenier@priv.gc.ca>
2020-08-31 15:06:27 -0400

    Now modifying the Owner initials trhough some json manipulation (works but in porgress)

---
+++ src/Compliance.Plugins/TimelineRecords.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Compliance.Plugins
+{
+    [Serializable]
+    public class TimelineRecords
+    {
+        public dynamic[] Entities { get; set; }
+    }
+}
--- src/Compliance.Plugins/TimelineTransformationsPlugin.cs
+++ src/Compliance.Plugins/TimelineTransformationsPlugin.cs
@@ -1,7 +1,10 @@
 using Compliance.Entities;
+using DocumentFormat.OpenXml.Office2013.PowerPoint.Roaming;
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
 using Microsoft.Xrm.Sdk;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Script.Serialization;
 
 namespace Compliance.Plugins
 {
@@ -43,6 +46,7 @@ namespace Compliance.Plugins
                     }
                     break;
                 case PluginMessage.RetrieveTimelineWallRecords:
+                    // Pre-fetch of results
                     if (localContext.PluginExecutionContext.InputParameters.ContainsKey("FetchXml"))
                     {
                         var inputParams = localContext.PluginExecutionContext.InputParameters;
@@ -54,6 +58,48 @@ namespace Compliance.Plugins
                         inputParams.Remove("FetchXml");
                         inputParams.Add(new KeyValuePair<string, object>("FetchXml", newFetchXmlValue));
                     }
+
+                    // https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to
+                    // Post-fetch of results
+                    if (localContext.PluginExecutionContext.OutputParameters.ContainsKey("TimelineWallRecords"))
+                    {
+                        var serializer = new JavaScriptSerializer();
+                        var timelineWallRecords = serializer.Deserialize<TimelineRecords>(localContext.PluginExecutionContext.OutputParameters["TimelineWallRecords"]?.ToString());
+
+                        foreach (var entity in timelineWallRecords.Entities)
+                        {
+                            //TODO: if (entity["LogicalName"] != "opc_event") continue;
+
+                            // This or regex? I would still need to be in the context of one entity at a time to not replace the wrong ownerid attribute but due to nested curly braces not sure
+
+                            // Could most likely be a cleaner and statically typed way here but it's already pretty simple
+                            dynamic createdOnBehalfAttribute = null;
+                            dynamic ownerAttribute = null;
+
+                            foreach (var attribute in entity["Attributes"])
+                            {
+                                if (attribute["Key"] == "opc_event_createdonbehalfby")
+                                    createdOnBehalfAttribute = attribute;
+
+                                if (attribute["Key"] == "ownerid")
+                                    ownerAttribute = attribute;
+                            }
+
+                            if (createdOnBehalfAttribute != null && ownerAttribute != null)
+                            {
+                                ownerAttribute["Value"] = createdOnBehalfAttribute["Value"]["Value"];
+                            }
+
+                            // Regex really simple pseudo code Considering we can somehow iterate over a string of the represented entity..
+
+                            // Find the opc_event_createdonbehalf with the expression and capture the "value" of the "value" group
+
+                            // Find the ownerid and replace the "value" group with the found value of opc_eventcreatedonbehalf
+                        }
+
+                        localContext.PluginExecutionContext.OutputParameters["TimelineWallRecords"] = serializer.Serialize(timelineWallRecords);
+                    }
+
                     break;
                 default:
                     break;
@@ -68,6 +114,7 @@ namespace Compliance.Plugins
             {
                 var annotation = entity.ToEntity<Annotation>();
 
+                entity["ownerid"] = annotation.CreatedBy;
                 entity["modifiedby"] = annotation.CreatedBy;
                 entity["modifiedon"] = annotation.CreatedOn;
             }
