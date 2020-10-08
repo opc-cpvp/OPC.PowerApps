080ff030ad04ef241ccc55cfd517fbb15fd43a9f
Joshua Campbell <joshcampbell191@gmail.com>
2020-10-07 21:45:53 -0400

    Added comments to provide context to additional definitions.

--- src/Compliance.WebResources/@types/sinonextensions/sinonextensions.d.ts
+++ src/Compliance.WebResources/@types/sinonextensions/sinonextensions.d.ts
@@ -1,11 +1,14 @@
 import "sinon";
 
+// This was put in place to minimize the changes to unit tests when introducing eslint.
 declare module "sinon" {
+    // TODO: These definitions should be removed in favor of proper definitions for spies
     interface SinonSpyStatic {
         <T>(obj: T): T;
         <T>(obj: T, method: string): SinonSpy;
     }
 
+    // TODO: This definition should be removed in favor of proper definitions for stubs.
     interface SinonStubStatic {
         <T>(obj: T, method: string): SinonStub;
     }
