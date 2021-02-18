6bfc9cef2777960a84db41fa4c5bd498bfe94654
Stéphanie Brouillard <Stephanie.Brouillard@priv.gc.ca>
2021-02-18 08:53:32 -0500

    Added option to open from allegation grid / changed from issue main form to quick create form

--- src/Compliance.WebResources/src/handlers/Commands/OpenIssueDialogCommandHandler.ts
+++ src/Compliance.WebResources/src/handlers/Commands/OpenIssueDialogCommandHandler.ts
@@ -1,46 +1,97 @@
 import { injectable, inject } from "inversify";
-import { ICommandHandler, ExtendedXrmPageBase, IAllegationTypeService } from "../../interfaces";
+import { ICommandHandler, ExtendedXrmPageBase, IAllegationTypeService, IAllegationService } from "../../interfaces";
 
 @injectable()
 export class OpenIssueDialogCommandHandler implements ICommandHandler<ExtendedXrmPageBase> {
     private _xrmNavigation: Xrm.Navigation;
+    private _allegationService: IAllegationService;
     private _allegationTypeService: IAllegationTypeService;
+    private _issueParameters: { [key: string]: string };
 
     constructor(
         @inject(nameof<Xrm.Navigation>()) xrmNavigation: Xrm.Navigation,
-        @inject(nameof<IAllegationTypeService>()) allegationTypeService: IAllegationTypeService
+        @inject(nameof<IAllegationTypeService>()) allegationTypeService: IAllegationTypeService,
+        @inject(nameof<IAllegationService>()) allegationService: IAllegationService
     ) {
         this._xrmNavigation = xrmNavigation;
         this._allegationTypeService = allegationTypeService;
+        this._allegationService = allegationService;
+        this._issueParameters = {};
     }
 
     async execute<TForm extends ExtendedXrmPageBase>(formContext: TForm): Promise<void> {
-        const parameters: { [key: string]: string } = {};
+        let acceptableAllegations: Xrm.GridRow<string>[];
 
-        if (formContext.data.entity.getEntityName() === "opc_allegation") {
-            const allegationTypeId = formContext.getAttribute("opc_allegationtypeid").getValue()[0].id;
-            const allegationType = await this._allegationTypeService.getAllegationType(allegationTypeId);
+        if (formContext.data.entity.getEntityName() === "opc_complaint") {
+            this._issueParameters.opc_complaintid = formContext.data.entity.getId();
 
-            parameters.opc_allegationtypeid = allegationTypeId;
-            parameters.opc_allegationtypeidname = allegationType.opc_name;
+            const rows = formContext.getControl("subgrid_allegations").getGrid().getSelectedRows();
+
+            acceptableAllegations = rows.get((attribute, index) => {
+                const disposition = attribute.getData().getEntity().attributes.getByName("opc_disposition").getValue();
+
+                // Only consider selected allegations with 'Acceptable' dispositions
+                if (disposition && disposition.toString() === opc_allegationdisposition.Acceptable.toString()) {
+                    return true;
+                }
+
+                return false;
+            });
         }
 
-        this._xrmNavigation
-            .navigateTo(
-                {
-                    pageType: "entityrecord",
-                    entityName: "opc_issue",
-                    data: parameters
-                },
-                {
-                    target: Xrm.NavigationOptionsTarget.Dialog,
-                    position: Xrm.NavigationOptionsPosition.Center,
-                    width: { value: 75, unit: "%" },
-                    height: { value: 75, unit: "%" }
+        if (formContext.data.entity.getEntityName() === "opc_allegation") {
+            const allegationId = formContext.data.entity.getId();
+            await this.setParamsInIssueForm(allegationId, formContext);
+
+            this._issueParameters.opc_complaintid = formContext.getAttribute("opc_complaintid").getValue()[0].id;
+        }
+        // There's only one allegation so we can prefill the Issue form
+        else if (acceptableAllegations?.length === 1) {
+            const allegationId = acceptableAllegations[0].data.getEntity().getId();
+            await this.setParamsInIssueForm(allegationId);
+        }
+
+        const entityFormOptions = {
+            entityName: "opc_issue",
+            useQuickCreateForm: true
+        };
+
+        try {
+            this._xrmNavigation.openForm(entityFormOptions, this._issueParameters).then(result => {
+                const issueId = result.savedEntityReference[0].id;
+                // link the new issue with the selected allegations (N:N)
+                if (acceptableAllegations) {
+                    acceptableAllegations.forEach(x => this._allegationService.updateAllegation(x.data.entity.getId(), issueId));
+                } else {
+                    this._allegationService.updateAllegation(this._issueParameters.opc_allegationid, issueId);
                 }
-            )
-            .catch(() => {
-                // TODO catch error
             });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    private async setParamsInIssueForm(allegationId: string, formContext?: ExtendedXrmPageBase): Promise<void> {
+        let accessRequestNumber: string;
+        let allegationTypeId: string;
+
+        if (formContext && formContext.data.entity.getEntityName() === "opc_allegation") {
+            accessRequestNumber = formContext.getAttribute("opc_referencenumber").getValue();
+            allegationTypeId = formContext.getAttribute("opc_allegationtypeid").getValue()[0].id;
+        } else {
+            const allegation = await this._allegationService.getAllegation(allegationId);
+            accessRequestNumber = allegation.opc_referencenumber;
+            allegationTypeId = allegation.opc_allegationtypeid_guid;
+        }
+
+        const allegationType = await this._allegationTypeService.getAllegationType(allegationTypeId);
+
+        if (accessRequestNumber) {
+            this._issueParameters.opc_accessrequestnumber = accessRequestNumber;
+        }
+
+        this._issueParameters.opc_allegationtypeid = allegationTypeId;
+        this._issueParameters.opc_allegationtypeidname = allegationType.opc_name;
+        this._issueParameters.opc_allegationid = allegationId;
     }
 }
