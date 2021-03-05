753f4e6833397305632be1c4027fd7efe63394d5
Jonathan Thomassin <sargatos@gmail.com>
2021-03-05 16:25:13 -0500

    Features/multiselect bpf (#134)
    * First pass of changes for the bpf flyouts.
    
    * Small changes to make the tests work. Removed space.
    
    * Changes to mutationObserver for the bpf.
    Reduce the width of the flyout by 50px.
    Added code for the stage dock.
    Added comments.
    
    * Manually added new field in bpf stage
    
    * Removed the max-height for the BPF Stage Flyout.
    
    * Removed startsWith character in query. Added more comments.
    
    Co-authored-by: Jonathan Thomassin <jonathan.thomassin@priv.gc.ca>

--- src/Compliance.WebResources/src/forms/AccessRequestDocumentQuickCreateForm.ts
+++ src/Compliance.WebResources/src/forms/AccessRequestDocumentQuickCreateForm.ts
@@ -1,13 +1,13 @@
 import { injectable } from "inversify";
 import "reflect-metadata";
-import { IPowerForm } from "../interfaces";
+import { PowerForm } from "./PowerForm";
 import { XrmHelper } from "../helpers/XrmHelper";
 
 // @see https://github.com/typescript-eslint/typescript-eslint/issues/2573
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export namespace AccessRequestDocument.Forms {
     @injectable()
-    export class QuickCreate implements IPowerForm<Form.opc_accessrequestdocument.QuickCreate.QuickCreate> {
+    export class QuickCreate extends PowerForm<Form.opc_accessrequestdocument.QuickCreate.QuickCreate> {
         /**
          * Handle the form OnLoad event.
          *
@@ -16,6 +16,8 @@ export namespace AccessRequestDocument.Forms {
         public initializeComponents(
             initializationContext: Xrm.ExecutionContext<Form.opc_accessrequestdocument.QuickCreate.QuickCreate, any>
         ): void {
+            super.initializeComponents(initializationContext);
+
             const formContext = initializationContext.getFormContext() as Form.opc_accessrequestdocument.QuickCreate.QuickCreate;
 
             formContext.getAttribute("opc_contestedbycomplainant").addOnChange(x => this.contestedByComplainant_OnChange(x));
