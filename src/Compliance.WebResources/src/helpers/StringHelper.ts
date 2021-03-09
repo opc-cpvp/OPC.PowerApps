afd5cbd97eadc2c4941111592ba0a8dfcf91ca2e
Stéphanie Brouillard <Stephanie.Brouillard@priv.gc.ca>
2021-03-09 11:07:04 -0500

    Refactored formatGuid helper

--- src/Compliance.WebResources/src/helpers/StringHelper.ts
+++ src/Compliance.WebResources/src/helpers/StringHelper.ts
@@ -5,9 +5,11 @@
 
     // Format Guid without brackets and to lower case
     public static formatGuid(guid: string): string {
-        const regex = new RegExp("^{.*?}$");
-        if (regex.test(guid)) {
-            return guid.slice(1, guid.length - 1).toLowerCase();
+        const regex = new RegExp("^{?(.*?)}$");
+        const result = regex.exec(guid);
+
+        if (result) {
+            return result[1].toLowerCase();
         }
         return guid;
     }
